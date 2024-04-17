267faaa3e9ee0bb7710a183740b9533799aa4776
jkool702 <107448833+jkool702@users.noreply.github.com>
2024-04-16 23:19:29 -0400

    Update build_instructions.sh

--- build_instructions.sh
+++ build_instructions.sh
@@ -1,14 +1,44 @@
 #!/usr/bin/env bash
 
+# # # # # USER SPECIFIED OPTIONS
+
+# set main buildroot
 buildroot_dir="/mnt/ramdisk"
-bin_dir="${buildroot_dir}"/openwrt-custom-builds/WRX36/bin
 
-# NOTE; In my github repo openwrt fork, I have corrected the listing for the dynalink so that the SOC is listed as ipq8074, not ipq8072. This may or may not have had an impact on the resulting image.
+# set paths where you can find previous builds diffconfig, kernel config, and "files" directoryu needs to have: "configs/.config.diff", "configs/.config.kernel", and "files")
+# if you dont some/any of these then comment out (or unset if you already ran) the ones you dont have
+prev_files_dir_path="${buildroot_dir}"/openwrt-custom-builds/WRX36/bin/extra/files
+prev_diff_config_path="${buildroot_dir}"/openwrt-custom-builds/WRX36/bin/extra/configs/.config.diff
+prev_kernel_config_path="${buildroot_dir}"/openwrt-custom-builds/WRX36/bin/extra/configs/.config.kernel
 
-# make build directory and clone github repo
+# ALT -if you dont have a previous diffconfig (.config.diff) but have a previous full config (.config), specify its path below. Be sure to comment out (dont set) the above line setting "prev_diff_config_path"
+# prev_full_config_path="${buildroot_dir}"/openwrt-custom-builds/WRX36/bin/extra/configs/.config
+
+# choose whether or not to build on a tmpfs
+tmpfs_build_flag=true
+
+# # # # # BEGIN SCRIPT
+
+# set tmpfs_build_flag to false if empty+unset --OR-- to true if nonempty+set (to anything)
+tmpfs_build_flag="${tmpfs_build_flag:+true}"
+: "${tmpfs_build_flag:=false}"
+
+# make build directory 
 mkdir -p "${buildroot_dir}"
-cat /proc/mounts | grep -F "${buildroot_dir}" | grep -q 'tmpfs' || sudo mount -t tmpfs tmpfs "${buildroot_dir}" -o defaults -o size=96G
 
+# mount tmpfs to it (if it doesnt already have one)
+# unless your system has a substantial amount of ram (64gb or more) you should probably comment out this line 
+# --OR-- you should set the "automatically remove build directories" (set "CONFIG_AUTOREMOVE=y" in .config)
+${tmpfs_build_flag} && {
+	 grep -F "${buildroot_dir}" </proc/mounts | grep -q 'tmpfs' || {
+	 	read -r memKB < /proc/meminfo
+		memKB="${memKB% *}"
+		memKB="${memKB#* }"
+	 	sudo mount -t tmpfs tmpfs "${buildroot_dir}" -o defaults -o size=$(( ( memKB * 1024 * 3 ) / 4 ))
+	}
+}
+
+# download custom firmware repo (to grab config files) to "${buildroot_dir}"/openwrt-custom-builds
 cd "${buildroot_dir}"
 git clone https://github.com/jkool702/openwrt-custom-builds.git --branch=dynalink_wrx36_NSS_build
 
@@ -24,37 +54,68 @@ git clone --depth 1 --branch master --single-branch --no-tags --recurse-submodul
 # install packages from feeds
 ./scripts/feeds update -a
 ./scripts/feeds install -a
-./scripts/feeds install -a;
+./scripts/feeds install -a
 
-# move in files and configs
-cp -r "${bin_dir}"/extra/files ./
-cp "${bin_dir}"/extra/configs/{.config,.config.kernel} ./
+# move in files and configs from previous build
+[[ "${prev_files_dir_path}" ]] && [[ -d "${prev_files_dir_path}" ]] && cp -r "${prev_files_dir_path}" ./
+[[ "${prev_kernel_config_path}" ]] && [[ -f "${prev_kernel_config_path}" ]] && cp "${prev_kernel_config_path}" .config.kernel.prev || touch .config.kernel.prev
+if [[ "${prev_diff_config_path}" ]] && [[ -f "${prev_diff_config_path}" ]]; then
+	cp "${prev_diff_config_path}" .config
+elif [[ "${prev_full_config_path}" ]] && [[ -f "${prev_full_config_path}" ]]; then
+	cp "${prev_full_config_path}" .config
+	./scripts/diffconfig > .config.diff
+	\mv -f .config.diff .config 
+fi
 
-# speed up downloads. Not needed if using my github repo
+# get target board
+target_board="$(grep -F CONFIG_TARGET_BOARD <.config | sed -E 's/^[^"]*"//;s/".*$//')"
+
+# make copy of default target config(s)
+for nn in find target/linux/${target_board}/config-*; do
+    cp $nn .config.target.orig0.${nn##*/}
+done
+
+# speed up downloads. Not needed if using my github repo.
 # echo "$(cat scripts/download.pl | sed -zE s/'(elsif \(\$mirror \=\~ \/\^\\\@GNU\\\/\(\.\+\)\$\/\) \{[^p]+push \@mirrors\, )[^\}]+\}'/'\1"https:\/\/gnu.mirror.constant.com\/$1"\n\t}'/)" > scripts/download.pl
 
-# configure openwrt settings
+# configure openwrt settings.
 make menuconfig
 
-# ALT:
+# generate diffconfig
+./scripts/diffconfig > .config.diff
+
+ grep -qE '^CONFIG_AUTOREMOVE=y$' <.config && config_autoremove_flag=true || config_autoremove_flag=false
+
+# ALT - run this instead of `make menuconfig` if you dont want to change anything.
 # make defconfig
 
 # download/check sources, then build unmodified kernel 
 make -j$(nproc) download 
 
-# fix package hash mis-matches
+# force fix package hash mis-matches
 C="$(make check | grep -B 1 PKG_MIRROR_HASH)"
 mapfile -t B < <(grep PKG_MIRROR_HASH <<<"$C" | sed -E s/'^.* '//)
 mapfile -t A < <(grep directory <<<"$C" | sed -E 's/^.* '"'"'//;s/'"'"'$//')
-
 for kk in "${!A[@]}"; do
     sed -iE s/'^PKG_MIRROR_HASH.*$'/'PKG_MIRROR_HASH:='"${B[$kk]}"'/' "${A[$kk]}"'/Makefile'
 done
 
-make -j$(nproc) V=sc check prepare 
+# confirm check doesnt flag anything (there should be no red output)
+make -j$(nproc) V=sc check  
 
-# save important paths in variables
-target_board="$(grep -F CONFIG_TARGET_BOARD <.config | sed -E 's/^[^"]*"//;s/".*$//')"
+# we need to disable automatic build directory removal for the kernel build (if it is set)
+if ${config_autoremove_flag}; then
+	# build tools+toolchain
+	make -j$(nproc) V=sc prepare_kernel_conf
+	cp .config .config.save
+	{ grep -vE '^CONFIG_AUTOREMOVE=y$' <.config.save; echo '# CONFIG_AUTOREMOVE is not set'; } >.config
+fi
+
+# build [tools+toolchain+]kernel (unmodified default kernel)
+make -j$(nproc) V=sc prepare 
+
+# save important target/kernel config paths in variables
+# if the target supports 2 different kernel versions this *should* correctly choose the one that you are using
 target_kconfig="$(find target/linux/${target_board}/config-* -maxdepth 0)"
 (( $(wc -l <<<"${target_kconfig}") > 1 )) && {
 	if grep -qF 'CONFIG_TESTING_KERNEL=y' <.config; then
@@ -74,52 +135,63 @@ builddir_kernel=(build_dir/target*/linux-${target_board}*/linux-${target_kconfig
 builddir_kernel="$(realpath "${builddir_kernel}")"
 builddir_kconfig="${builddir_kernel}"/.config
 
+# copy the target config after menuconfig configuring
+cp "$target_kconfig" .config.target.orig0
+
 # copy full default config of the (just built) default kernel
 \mv -f "${builddir_kconfig}" .config.kernel.orig
 
-# generate the partial .config (for the kernel that kernel_menuconfig by default shows) in the kernel build dir
-# NOTE: ***DONT CHANGE ANYTHING*** in this kernel_menuconfig - immediately save it (to .config) and exit. 
-make kernel_menuconfig
+# copy the full path of the orig kernel config (i.e., copy (ctrl+shift+c) the output of the below command)
+realpath .config.kernel.orig
 
-# diff and add to target config to get target diffconfig of full default kernel config
-diff .config.kernel.orig "${builddir_kconfig}" | grep '<' | sed -E s/'^< '// | tee -a .config.kernel.diff0 
-cat .config.kernel.diff0 >> "${target_kconfig}"
+# generate the partial changes to the target config-x.y by loading the .config from the default kernel build
+# NOTE: ***DONT CHANGE ANYTHING*** in this kernel_menuconfig!!!! load the ".config.kernel.orig" and save as ".config" exit. 
+# go to "LOAD", paste the path of the ".config.kernel.orig" that you just copied, press enter, then save as ".config" and exit.
+# this will automatically add required items to the target config-x.y
+make target/linux/clean kernel_menuconfig
 
-# do this a second time to pull in all the changes in my ciustom kernel
-diff ${bin_dir}/extra/configs/.config.kernel .config.kernel.orig | grep '<' | sed -E s/'^< '// | tee -a .config.kernel.diff1 
-cat .config.kernel.diff1 >> "${target_kconfig}"
+# copy the target (post-menuconfig with all automatic changes) target config
+cp ${target_kconfig} .config.target.orig
 
-# remove incomplete .config out of kernel build dir
-\rm -f  "${builddir_kconfig}" 
+# generate various diffs to layer on the the original full kernel config
 
-# regenerate .config (which gives the full complete kernel .config + my modifictions) and save as ".config" 
-# OPTIONAL: actually custom configure the kernel this time 
-make kernel_menuconfig
+# diff orig full kernel config and kernel config downloaed from repo
+diff .config.kernel.prev .config.kernel.orig | grep -E '^<' | sed -E s/'^< '// > .config.kernel.prev.diff
 
-# diff and add to target config to get diffconfig of full custom kernel config,
-# then diff that with the target config template (that make kernel_menuconfig 
-# *should* have updated) and add anything that is missing to target .config.
-# note: diff arguments of inner diff are flipped from previous diff command
-echo "$(diff <(diff "${builddir_kconfig}" ${bin_dir}/extra/configs/.config.kernel | grep '<' | sed -E s/'^< '//) "${target_kconfig}" | grep '<' | sed -E s/'^< '//)" | tee -a .config.kernel.diff
-cat .config.kernel.diff >> "${target_kconfig}"
+# diff original (pre-menuconfig) target config and full (post-menuconfig with all automatic changes) target config
+diff .config.target.orig0.${target_kconfig##*/} .config.target.orig | grep -E '^<' | sed -E s/'^< '// > .config.target.orig.diff
 
-# save full .config and fix up target config. 
-# dont change anything in kernel_menuconfig - immediately save it (to .config) and exit
-\mv -f "${builddir_kconfig}" .config.kernel
-make kernel_menuconfig
+# layer full kernel config --> differernces to kernel config from repo --> differences to target config based on menuconfig choices
+# the idea here is that automatic changesd to the kernel probably are being made for a good reason,
+# so those will take precedence over changes to make the kernel config match the one downloaded from the github repo
+cat .config.kernel.orig .config.kernel.prev.diff .config.target.orig.diff > .config.kernel
 
-# check that re-generating the kernel .config doesnt change anything
-# the following command should indicate there are no changes
-diff .config.kernel "${builddir_kconfig}"
+# copy the full path of the unprocessed kernel config (i.e., copy (ctrl+shift+c) the output of the below command)
+realpath .config.kernel
 
-# make kernel
-make -j$(nproc) V=sc prepare
+# generate the changes to the target config-x.y required to incorporate changes from the downloaded kernel config from the repo
+# NOTE: ***BEFORE CHANGING ANYTHING***:  load the ".config.kernel" 
+# go to "LOAD", paste the path of the ".config.kernel" that you just copied, press enter
+# NOW YOU CAN CHANGE STUFF (should you want to...you dont *have* to further tweak the kernel)
+# After loading the .config.kernel and (optionally) further tweaking the kernel, save as ".config" and exit. 
+# All required changes will be automatically saved to the target .config-x.y
+make kernel_menuconfig 
 
-# add missing kernel module info to build
+# rebuild kernel with all kernel config tweaks present
+make -j$(nproc) V=sc target/linux/clean prepare
+
+# copy final target and kernel config
+mv .config.kernel .config.kernel.old
+cp "$builddir_kconfig" .config.kernel
+cp "$target_kconfig" .config.target
+
+# add missing kernel module info to build by including them in the files/lib/modules/<kernel_ver> (this makes `depmod` work better)
 mkdir -p files/lib/modules/"${builddir_kernel##*/linux-}"
+\cp -f "${builddir_kernel}"/{,.}{m,M}od* files/lib/modules/"${builddir_kernel##*/linux-}"
+find files/lib/modules/"${builddir_kernel##*/linux-}" -type f -empty -exec rm {} +
 
-\cp -f "${builddir_kernel}"/{m,M,.m,.M}od* files/lib/modules/"${builddir_kernel##*/linux-}"
-\rm -f $(find files/lib/modules/"${builddir_kernel##*/linux-}" -type f -empty)
+# we need to re-enable automatic build directory removal for the rest of the build (if CONFIG_AUTOREMOVE set)
+${config_autoremove_flag} && \mv -f .config.save .config
 
 # make the rest of the build
 make -j$(nproc) -k V=sc
@@ -128,6 +200,6 @@ make -j$(nproc) -k V=sc
 mkdir -p bin/extra/keys
 mkdir -p bin/extra/configs
 cp key-build* bin/extra/keys
-cp .config* "$target_kconfig" bin/extra/configs
+cp .config* bin/extra/configs
 cp -r files bin/extra
 
